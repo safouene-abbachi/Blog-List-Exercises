0934b85bc659088283658283090bfabe4a7ee9bb
safouene <safouene-abbachi@hotmail.fr>
2022-08-22 15:49:43 +0100

    DEV : 4.2 Blog list, step2

--- server.js
+++ server.js
@@ -1,39 +1,6 @@
-require('dotenv').config();
-const http = require('http');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const mongoUrl = process.env.MONGODB_URI;
-const PORT = process.env.PORT;
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+const app = require('./app');
+const config = require('./utils/config');
 
-const Blog = mongoose.model('Blog', blogSchema);
-
-mongoose.connect(mongoUrl);
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
 });
