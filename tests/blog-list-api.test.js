3f496610c86844aa5f5ce5fb893cb78465bde7fc
safouene <safouene-abbachi@hotmail.fr>
2022-08-28 17:01:06 +0100

    DEV : 4.9*: Blog list tests, step2

--- tests/blog-list-api.test.js
+++ tests/blog-list-api.test.js
@@ -17,6 +17,12 @@ describe('When having initial blogs', () => {
     const response = await api.get('/api/blogs');
     expect(response.body).toHaveLength(helper.initialBlogs.length);
   }, 100000);
+
+  test('blogs must have id property', async () => {
+    const response = await api.get('/api/blogs');
+
+    expect(response.body[0]['id']).toBeDefined;
+  });
 });
 
 afterAll(() => {
